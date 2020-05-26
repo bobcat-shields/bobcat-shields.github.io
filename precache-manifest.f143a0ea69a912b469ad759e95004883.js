9f9a7997f9e8d969eb4c9730a825f86400c961c4
Bob Shields <bobcatshields@gmail.com>
2020-05-26 10:50:02 -0400

    Updates

---
+++ precache-manifest.f143a0ea69a912b469ad759e95004883.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0f66c2b923654a4ce418ebadb6568d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5df0057c7ca271a74811",
+    "url": "/static/css/main.47af08f1.chunk.css"
+  },
+  {
+    "revision": "fc3ac41de5c3f88029aa",
+    "url": "/static/js/2.e83f09a5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e83f09a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5df0057c7ca271a74811",
+    "url": "/static/js/main.3b41fe85.chunk.js"
+  },
+  {
+    "revision": "ad231d8fabbe2bf46077",
+    "url": "/static/js/runtime-main.7c8abbec.js"
+  }
+]);
